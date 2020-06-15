9925b0c987d4c0779b2051ca566c9540aadb78d7
Dev <udev@MacBook-Pro-de-Dev.local>
2020-06-15 14:53:58 +0200

    Set project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="ytspectro",
+    version="0.0.1",
+    url = 'https://github.com/jdlozanom/youtube-to-spectrogram',
+    description="Spectrogram maker from youtube audios",
+    author="Juan Diego Lozano",
+    license="MIT",
+    packages=['ytspectro'],
+)
